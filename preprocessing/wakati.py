a0375dddc307fab594ae485fdabc8a3566456845
yut-kt <yut.kt59@gmail.com>
2019-05-12 02:02:39 +0900

    update: 形態素解析結果にタブが複数入っている場合の対応

--- preprocessing/wakati.py
+++ preprocessing/wakati.py
@@ -24,7 +24,7 @@ def get_wakati_list(sentence: str) -> List[str]:
     for result in mecab.parse(sentence).split('\n'):
         if result == 'EOS':
             break
-        morpheme, morpheme_info = result.split('\t')
+        morpheme, morpheme_info = result.split('\t', maxsplit=1)
         word_class, _ = morpheme_info.split(',', maxsplit=1)
         if word_class != '記号':
             wakati.append(morpheme)
